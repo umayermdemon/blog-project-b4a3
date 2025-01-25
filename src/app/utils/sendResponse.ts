e2813d1c5d8a6e2dae9e2a37fac024fecad0d418
umayermdemon <mamudmdemon@gmail.com>
2025-01-25 08:33:11 +0600

    sendResponse updated

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -11,6 +11,7 @@ const sendResponse = <T>(res: Response, data: TResponse<T>) => {
   res.status(data?.statusCode).json({
     success: data?.success,
     message: data?.message,
+    statusCode: data?.statusCode,
     data: data?.data,
   });
 };
