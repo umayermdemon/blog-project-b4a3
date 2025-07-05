1c663584011900c4a4560dd7312dd1834730f2a0
umayermdemon <mamudmdemon@gmail.com>
2025-07-05 08:12:40 +0600

    fix: push all

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app: Application = express();
 //
 // parsers
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173" }));
 
 // applications route
 app.use("/api", router);
